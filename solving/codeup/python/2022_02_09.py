0cb286aa5ba158ef82b10ec0dd1302a347e38c07
SOOYEON YOO <dbtndus0527@gmail.com>
2022-02-09 20:25:49 +0900

    Create 2022_02_09.py
    코드업 파이썬 기초 100제 풀기

---
+++ solving/codeup/python/2022_02_09.py
@@ -0,0 +1,42 @@
+## 6071
+```
+n = 1
+
+while n!=0 :
+    n = int(input())
+    if(n!=0):
+        print(n)
+```
+## 6072
+```
+n = int(input())
+
+while n>0:
+    print(n)
+    n-=1
+```
+## 6073
+```
+n = int(input())
+
+while(n!=0) :
+    print(n-1)
+    n-=1
+```
+## 6074
+```
+c = ord(input())
+basic = ord('a')
+
+while(basic <= c):
+    print(chr(basic), end=" ")
+    basic+=1
+```
+## 6075
+```
+n = int(input())
+p=0
+while(p<=n):
+    print(p)
+    p+=1
+```
